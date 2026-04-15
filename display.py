d15af38db8a6ad718e6e6a9638fe87b816cd8dfc
eolivier <ewan.olivierverdier1@gmail.com>
2026-04-15 16:00:05 +0200

    Finish

--- display.py
+++ display.py
@@ -23,17 +23,16 @@ def display(maze: List[List[int]], path: List[str], show_path: bool,
 
     W = "\033[47m  \033[0m" if not change_color else "\033[46m  \033[0m"
     MOTIF_W = "\033[100m  \033[0m"
-
     START = "\033[45m  \033[0m"
     EXIT = "\033[41m  \033[0m"
     PATH = "\033[42m  \033[0m"
     EMPTY = "  "
 
     nw_path = transform_path(path, entry) if path and show_path else []
+    f_path = [(entry[1], entry[0])] + nw_path
     entry_t = (entry[1], entry[0])
     exit_t = (exit_pos[1], exit_pos[0])
 
-    height = len(maze)
     width = len(maze[0])
 
     print(W * (width * 2 + 1))
@@ -49,24 +48,34 @@ def display(maze: List[List[int]], path: List[str], show_path: bool,
                 content = START
             elif curr == exit_t:
                 content = EXIT
-            elif curr in nw_path:
+            elif curr in f_path:
                 content = PATH
             elif char == 15:
                 content = MOTIF_W
             else:
                 content = EMPTY
 
-            is_motif = (char == 15)
+            east_wall = (char & 2) != 0
+            south_wall = (char & 4) != 0
 
-            east_color = MOTIF_W if is_motif else W
-            south_color = MOTIF_W if is_motif else W
-            corner_color = MOTIF_W if is_motif else W
+            next_east = (y, x + 1)
+            if not east_wall and curr in f_path and next_east in f_path:
+                east_color = PATH
+            else:
+                east_color = MOTIF_W if (char == 15)\
+                    else W if east_wall else EMPTY
 
-            east_wall = (char & 2) or (x + 1 < width and (maze[y][x+1] & 8))
-            south_wall = (char & 4) or (y + 1 < height and (maze[y+1][x] & 1))
+            next_south = (y + 1, x)
+            if not south_wall and curr in f_path and next_south in f_path:
+                south_color = PATH
+            else:
+                south_color = MOTIF_W if (char == 15)\
+                 else W if south_wall else EMPTY
 
-            mid_line += content + (east_color if east_wall else content)
-            bot_line += (south_color if south_wall else content) + corner_color
+            corner_color = MOTIF_W if (char == 15) else W
+
+            mid_line += content + east_color
+            bot_line += south_color + corner_color
 
         print(mid_line)
         print(bot_line)
